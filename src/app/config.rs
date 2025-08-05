df5619a0a4f264eb734667332c12d6db7db193e9
CeerDecy <1748788674@qq.com>
2025-08-05 15:50:13 +0800

    refactor(config): boot application with bootstrap.toml

--- src/app/config.rs
+++ src/app/config.rs
@@ -1,7 +1,27 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone, Default)]
+pub struct AppConfig {
+    #[serde(default)]
+    pub mcp_center: McpCenter,
+    #[serde(default)]
+    pub mcp_registry: McpRegistry,
+}
+
+#[derive(Deserialize, Debug, Clone, Default)]
 pub struct McpCenter {
     #[serde(default)]
-    pub port: u16,
+    pub http_port: u16,
+    #[serde(default)]
+    pub grpc_port: u16, // TODO support grpc
+}
+
+#[derive(Deserialize, Debug, Clone, Default)]
+#[serde(tag = "type")]
+pub enum McpRegistry {
+    #[default]
+    #[serde(rename = "memory")]
+    LocalMemory,
+    #[serde(rename = "external")]
+    External { url: String, token: Option<String> },
 }
