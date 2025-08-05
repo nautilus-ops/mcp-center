3d16d06a4c7adcd79107c4033e215109d0d6ca2f
CeerDecy <1748788674@qq.com>
2025-08-05 15:48:40 +0800

    update gitignore

--- src/envs/mod.rs
+++
@@ -1,7 +0,0 @@
-pub const REGISTRY_TYPE: &str = "REGISTRY_TYPE";
-
-/// external api url
-pub const EXTERNAL_API: &str = "EXTERNAL_API";
-
-/// external api authorization key
-pub const EXTERNAL_API_AUTHORIZATION: &str = "EXTERNAL_API_AUTHORIZATION";
