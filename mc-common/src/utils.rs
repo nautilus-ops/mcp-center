1a1cfc34e6e36a0ed05cc8a1c1de9418334e217d
CeerDecy <1748788674@qq.com>
2025-08-26 00:07:12 +0800

    feat: add some test for utils

--- mc-common/src/utils.rs
+++ mc-common/src/utils.rs
@@ -20,3 +20,41 @@ pub fn replace_env_variables(input: String) -> String {
     })
     .into_owned()
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use crate::utils::replace_env_variables;
+
+    #[test]
+    fn test_replace_env_variables() {
+        struct TestCase {
+            input: &'static str,
+            want: &'static str,
+        }
+
+        let tests = vec![
+            TestCase {
+                input: r#"self_addr = "${SELF_ADDR:http://127.0.0.1}""#,
+                want: r#"self_addr = "http://127.0.0.1""#,
+            },
+            TestCase {
+                input: r#"mcp_definition_path = "${SERVER_DEFINITION_PATH:mcp_servers.toml}""#,
+                want: r#"mcp_definition_path = "mcp_servers.toml""#,
+            },
+            TestCase {
+                input: r#"port = "${POSTGRES_PORT:5432}""#,
+                want: r#"port = 5432"#,
+            },
+            TestCase {
+                input: r#"host = "${POSTGRES_HOST}""#,
+                want: r#"host = "127.0.0.1""#,
+            },
+        ];
+
+        unsafe { env::set_var("POSTGRES_HOST", "127.0.0.1") }
+        tests
+            .into_iter()
+            .for_each(|t| assert_eq!(replace_env_variables(t.input.to_string()), t.want));
+    }
+}
